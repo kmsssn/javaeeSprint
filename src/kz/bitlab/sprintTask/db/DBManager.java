cf227ab14c37f7d1e3cd182771ce38438294a0cb
kmsssn <aminaashim0@gmai.com>
2023-04-07 02:26:03 +0600

    Amina molodec1

--- src/kz/bitlab/sprintTask/db/DBManager.java
+++ src/kz/bitlab/sprintTask/db/DBManager.java
@@ -6,10 +6,10 @@ public class DBManager {
     public static final ArrayList<Tasks> tasks=new ArrayList<>();
     private static Long id=5L;
     static{
-        tasks.add(new Tasks(1L, "Создать веб-приложение на Java EE", "23.10.2021", "Да"));
-        tasks.add(new Tasks(2L, "Убраться дома и закупить продукты", "25.10.2021", "Да"));
-        tasks.add(new Tasks(3L, "Выполнить все домашние задания", "28.10.2021", "Нет"));
-        tasks.add(new Tasks(4L, "Записаться на качку", "12.12.2021", "Нет"));
+        tasks.add(new Tasks(1L, "Создать веб-приложение на Java EE", "-", "23.10.2021", "Да"));
+        tasks.add(new Tasks(2L, "Убраться дома и закупить продукты", "-", "25.10.2021", "Да"));
+        tasks.add(new Tasks(3L, "Выполнить все домашние задания", "-", "28.10.2021", "Нет"));
+        tasks.add(new Tasks(4L, "Записаться на качку", "-","12.12.2021", "Нет"));
     }
     public static ArrayList<Tasks> getAllTasks(){
         return tasks;
